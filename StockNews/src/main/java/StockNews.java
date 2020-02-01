e1b965af1adcce27efc04db5f44438e2e0b918de
Henry <hdykhne@hotmail.ca>
2020-01-31 21:28:05 -0500

    modified project structure and implemented checkstyle and cl

--- StockNews/src/main/java/StockNews.java
+++
@@ -1,5 +0,0 @@
-public class StockNews {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/StockNews.java
@@ -0,0 +1,23 @@
+
+/**
+ * This is an app to view stocks and relevant news.
+ *
+ * @version 0.1
+ */
+final class StockNews {
+
+    /**
+     * Class should not be instantiated
+     **/
+    private StockNews() {
+        throw new AssertionError("Instantiating utility class...");
+    }
+
+    /**
+     * Main method.
+     * @param args command line arguments
+     **/
+    public static void main(String[] args) {
+
+    }
+}
