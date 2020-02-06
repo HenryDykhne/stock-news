af4f62a0942c1973f1158e1ca9bb911a35f21d00
Henry <hdykhne@hotmail.ca>
2020-02-05 20:50:46 -0500

    checkstyle fixed

--- src/main/java/NewsFilter.java
+++ src/main/java/NewsFilter.java
@@ -1,6 +1,11 @@
 import news.pojo.Article;
 import news.pojo.NewsJSON;
-import java.util.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
 
 
 /**
@@ -22,18 +27,18 @@ public final class NewsFilter {
     public static List<Article> filterNews(NewsJSON newsJSON, Collection<Blacklist> blacklists) {
         //filtering nonactive blacklists for convenience
         Collection<String> activeMegaList = new HashSet<>();
-        for(Blacklist blacklist : blacklists){
-            if(blacklist.isActive()){
+        for (Blacklist blacklist : blacklists) {
+            if (blacklist.isActive()) {
                 activeMegaList.addAll(Arrays.asList(blacklist.getRestrictedText()));
             }
         }
 
         //collecting all articles that do NOT contain any restricted words from the active blacklists
         List<Article> filteredArticles = new ArrayList<>();
-        for(Article article : newsJSON.getArticles()) {
+        for (Article article : newsJSON.getArticles()) {
             filteredArticles.add(article);
-            for(String restriction : activeMegaList) {
-                if(article.getUrl().contains(restriction)) {
+            for (String restriction : activeMegaList) {
+                if (article.getUrl().contains(restriction)) {
                     filteredArticles.remove(article);
                 }
             }
